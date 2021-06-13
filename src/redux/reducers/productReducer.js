e21c9a817a21f171e99a16fb22407f178cbb47c1
suryatejaj97 <suryatejaj97@gmail.com>
2021-06-13 17:05:26 +0530

    final code changes

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -13,3 +13,15 @@ export const productReducer = (state= initialState, { type, payload }) => {
       return state;
   }
 };
+
+export const selectedProductsReducer = (state = {}, { type, payload }) => {
+  console.log(type);
+  switch (type) {
+    case ActionTypes.SELECTED_PRODUCT:
+      return { ...state, ...payload };
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return { };
+    default:
+      return state;
+  }
+}
